015042cbe2aa5eef85f767db36441bbb98877138
saqibkh <saqibkhan@utexas.edu>
2021-05-14 17:42:04 -0500

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/stock.py
+++ scripts/stock.py
@@ -32,31 +32,52 @@ class STOCK:
 
         # Make predictions/suggestions
         self.recommend_buying()
-        self.recommend_selling()
+        #self.recommend_selling()
 
     def recommend_buying(self):
-        i_percentage_change = ((self.current_stock_value - self.weighted_average) / (self.weighted_average)) * 100
-        if i_percentage_change < -25:
-            print("\n===================================================")
-            print("We recommend buying the following share: " + self.name)
-            print("Historic High: $" + str(self.highest_stock_value))
-            print("Historic Low: $" + str(self.lowest_stock_value))
-            print("Weighted Average: $" + str(self.weighted_average))
-            print("Current Price: $" + str(self.current_stock_value))
-            print("Percentage Change = " + str(i_percentage_change) + "%")
-            print("===================================================\n")
+        try:
+            if (self.weighted_average == 0) or (self.lowest_stock_value == 0) or (self.lowest_stock_value == 0):
+                pass
+            else:
+                i_percentage_change = ((self.current_stock_value - self.weighted_average)/(self.weighted_average)) * 100
+                i_percentage_difference_from_highest = ((self.current_stock_value - self.highest_stock_value)/(self.highest_stock_value)) * 100
+                i_percentage_difference_from_lowest = ((self.current_stock_value - self.lowest_stock_value) / (self.lowest_stock_value)) * 100
+                if (i_percentage_change < -25) and (i_percentage_difference_from_lowest < 5):
+                    print("\n===================================================")
+                    print("We recommend buying the following share: " + self.name)
+                    print("Historic High: $" + str(self.highest_stock_value))
+                    print("Historic Low: $" + str(self.lowest_stock_value))
+                    print("Weighted Average: $" + str(self.weighted_average))
+                    print("Current Price: $" + str(self.current_stock_value))
+                    print("Percentage Difference from average = " + str(i_percentage_change) + "%")
+                    print("Percentage Difference from highest = " + str(i_percentage_difference_from_highest) + "%")
+                    print("Percentage Difference from lowest = " + str(i_percentage_difference_from_lowest) + "%")
+                    print("===================================================\n")
+        except Exception as e:
+            raise Exception
 
     def recommend_selling(self):
-        i_percentage_change = ((self.weighted_average - self.weighted_average) / (self.weighted_average)) * 100
-        if i_percentage_change > 20:
-            print("\n===================================================")
-            print("We recommend selling the following share: " + self.name)
-            print("Historic High: $" + str(self.highest_stock_value))
-            print("Historic Low: $" + str(self.lowest_stock_value))
-            print("Weighted Average: $" + str(self.weighted_average))
-            print("Current Price: $" + str(self.current_stock_value))
-            print("Percentage Change = " + str(i_percentage_change) + "%")
-            print("===================================================\n")
+        try:
+            # Skip the stock if the weighted average is 0
+            if (self.weighted_average == 0) or (self.lowest_stock_value == 0) or (self.lowest_stock_value == 0):
+                pass
+            else:
+                i_percentage_change = ((self.current_stock_value - self.weighted_average)/(self.weighted_average)) * 100
+                i_percentage_difference_from_highest = ((self.current_stock_value - self.highest_stock_value)/(self.highest_stock_value)) * 100
+                i_percentage_difference_from_lowest = ((self.current_stock_value - self.lowest_stock_value)/(self.lowest_stock_value)) * 100
+                if (i_percentage_change > 25) and (i_percentage_difference_from_highest < 20):
+                    print("\n===================================================")
+                    print("We recommend selling the following share: " + self.name)
+                    print("Historic High: $" + str(self.highest_stock_value))
+                    print("Historic Low: $" + str(self.lowest_stock_value))
+                    print("Weighted Average: $" + str(self.weighted_average))
+                    print("Current Price: $" + str(self.current_stock_value))
+                    print("Percentage Difference from average = " + str(i_percentage_change) + "%")
+                    print("Percentage Difference from highest = " + str(i_percentage_difference_from_highest) + "%")
+                    print("Percentage Difference from lowest = " + str(i_percentage_difference_from_lowest) + "%")
+                    print("===================================================\n")
+        except Exception as e:
+            raise Exception
 
     def process_misc_data(self):
         self.highest_stock_value = max(self.HIGH,key=lambda x:float(x))
@@ -69,10 +90,21 @@ class STOCK:
 
     def process_data(self):
         for i in range(1, len(self.data)):
-            self.DATE.append(self.data[i][0])
-            self.OPEN.append(float(self.data[i][1]))
-            self.HIGH.append(float(self.data[i][2]))
-            self.LOW.append(float(self.data[i][3]))
-            self.CLOSE.append(float(self.data[i][4]))
-            self.ADJ_CLOSE.append(float(self.data[i][5]))
-            self.VOLUME.append(float(self.data[i][6]))
+
+            # It is possible that one of the entry is empty.
+            # Therefore copy the next entry into the current entry
+            if self.data[i][1] == '':
+                self.data[i] = self.data[i+1]
+                pass
+            else:
+                try:
+                    self.DATE.append(self.data[i][0])
+                    self.OPEN.append(float(self.data[i][1]))
+                    self.HIGH.append(float(self.data[i][2]))
+                    self.LOW.append(float(self.data[i][3]))
+                    self.CLOSE.append(float(self.data[i][4]))
+                    self.ADJ_CLOSE.append(float(self.data[i][5]))
+                    self.VOLUME.append(float(self.data[i][6]))
+                except Exception as e:
+                    x = self.data[i][1]
+                    raise Exception
