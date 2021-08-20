74ca4490d10735391c847ba3885102ec058f35ab
saqibkh <saqibkhan@utexas.edu>
2021-08-20 12:16:47 -0500

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/stock.py
+++ scripts/stock.py
@@ -13,6 +13,7 @@ import stock_constants
 import sim_logging
 from scipy.stats import linregress
 
+
 class STOCK:
     def __init__(self, i_name, i_file):
         self.simlog = sim_logging.SIMLOG()
@@ -43,7 +44,6 @@ class STOCK:
         self.market = ''
         self.shortName = ''
         self.longName = ''
-
 
         try:
             self.ticker_object = yf.Ticker(i_name)
@@ -96,29 +96,36 @@ class STOCK:
 
     def recommend_buying(self):
         try:
-            if (self.weighted_average == 0) or (self.lowest_stock_value == 0) or (self.highest_stock_value == 0):
+            if (self.weighted_average == 0) or (self.lowest_stock_value == 0) or (self.highest_stock_value == 0) or (
+                    self.weighted_average is None) or (self.lowest_stock_value is None) or (
+                    self.highest_stock_value is None):
                 self.simlog.warning("Unable to get one of the following values: "
-                             "weighted_average/lowest_stock_value/highest_stock_value")
+                                    "weighted_average/lowest_stock_value/highest_stock_value")
                 pass
             else:
-                i_percentage_change = ((self.current_stock_value - self.weighted_average)/(self.weighted_average)) * 100
+                i_percentage_change = ((self.current_stock_value - self.weighted_average) / (
+                    self.weighted_average)) * 100
                 self.simlog.debug("percentage_change = " + str(i_percentage_change) + "%")
 
                 i_percentage_difference_from_highest = ((self.current_stock_value - self.highest_stock_value) /
                                                         self.highest_stock_value) * 100
-                self.simlog.debug("percentage_difference_from_highest = " + str(i_percentage_difference_from_highest) + "%")
+                self.simlog.debug(
+                    "percentage_difference_from_highest = " + str(i_percentage_difference_from_highest) + "%")
 
                 i_percentage_difference_from_lowest = ((self.current_stock_value - self.lowest_stock_value) /
                                                        self.lowest_stock_value) * 100
-                self.simlog.debug("percentage_difference_from_lowest = " + str(i_percentage_difference_from_lowest) + "%")
+                self.simlog.debug(
+                    "percentage_difference_from_lowest = " + str(i_percentage_difference_from_lowest) + "%")
 
                 if ((i_percentage_change < -25) and (i_percentage_difference_from_lowest < 1)) or \
-                        ((i_percentage_change < -20) and (i_percentage_difference_from_lowest < 25) and (self.name in stock_constants.i_interesting_stocks)):
+                        ((i_percentage_change < -20) and (i_percentage_difference_from_lowest < 25) and (
+                                self.name in stock_constants.i_interesting_stocks)):
                     self.simlog.info("\n\n\n\n===================================================")
                     self.simlog.info("===================================================")
                     self.simlog.info("===================================================")
                     self.simlog.info("===================================================")
-                    self.simlog.info("We recommend buying the following share: " + self.shortName + "(" + self.name + ")")
+                    self.simlog.info(
+                        "We recommend buying the following share: " + self.shortName + "(" + self.name + ")")
 
                     # Wrap it with try/catch statement because it is not re-creatable in pycharm
                     try:
@@ -138,8 +145,10 @@ class STOCK:
                     self.simlog.info("Current Price: $" + str(self.current_stock_value))
                     self.simlog.info("Slope is equal to " + str(self.slope.slope))
                     self.simlog.info("Percentage Difference from average = " + str(i_percentage_change) + "%")
-                    self.simlog.info("Percentage Difference from highest = " + str(i_percentage_difference_from_highest) + "%")
-                    self.simlog.info("Percentage Difference from lowest = " + str(i_percentage_difference_from_lowest) + "%")
+                    self.simlog.info(
+                        "Percentage Difference from highest = " + str(i_percentage_difference_from_highest) + "%")
+                    self.simlog.info(
+                        "Percentage Difference from lowest = " + str(i_percentage_difference_from_lowest) + "%")
                     self.simlog.info("===================================================")
                     self.simlog.info("===================================================")
                     self.simlog.info("===================================================")
@@ -155,9 +164,12 @@ class STOCK:
                     or (self.lowest_stock_value == 0) or (self.name not in stock_constants.i_stocks_i_own):
                 pass
             else:
-                i_percentage_change = ((self.current_stock_value - self.weighted_average)/(self.weighted_average)) * 100
-                i_percentage_difference_from_highest = ((self.current_stock_value - self.highest_stock_value)/(self.highest_stock_value)) * 100
-                i_percentage_difference_from_lowest = ((self.current_stock_value - self.lowest_stock_value)/(self.lowest_stock_value)) * 100
+                i_percentage_change = ((self.current_stock_value - self.weighted_average) / (
+                    self.weighted_average)) * 100
+                i_percentage_difference_from_highest = ((self.current_stock_value - self.highest_stock_value) / (
+                    self.highest_stock_value)) * 100
+                i_percentage_difference_from_lowest = ((self.current_stock_value - self.lowest_stock_value) / (
+                    self.lowest_stock_value)) * 100
                 if (i_percentage_change > 25) and (i_percentage_difference_from_highest < 20):
                     print("\n\n\n\n===================================================")
                     print("===================================================\n")
@@ -187,14 +199,14 @@ class STOCK:
             raise Exception
 
     def process_misc_data(self):
-        self.highest_stock_value = max(self.HIGH,key=lambda x:float(x))
-        self.lowest_stock_value = min(self.LOW,key=lambda x:float(x))
-        self.current_stock_value = self.regularMarketPrice #(self.HIGH[-1] + self.LOW[-1]) / 2
+        self.highest_stock_value = max(self.HIGH, key=lambda x: float(x))
+        self.lowest_stock_value = min(self.LOW, key=lambda x: float(x))
+        self.current_stock_value = self.regularMarketPrice  # (self.HIGH[-1] + self.LOW[-1]) / 2
         self.slope = linregress(self.HIGH, list(range(1, len(self.DATE) + 1)))
 
         # Calculate the weighted average based on OPENING and CLOSING value
         for i in range(len(self.OPEN)):
-            self.weighted_average += self.OPEN[i]/float(len(self.OPEN))
+            self.weighted_average += self.OPEN[i] / float(len(self.OPEN))
 
     def process_data(self):
         for i in range(1, len(self.data)):
@@ -202,7 +214,7 @@ class STOCK:
             # It is possible that one of the entry is empty.
             # Therefore copy the next entry into the current entry
             if self.data[i][1] == '':
-                self.data[i] = self.data[i-1]
+                self.data[i] = self.data[i - 1]
                 pass
             else:
                 try:
