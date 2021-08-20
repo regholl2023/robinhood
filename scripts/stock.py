bb5a81963002a4ec77dc38c092e606aa2b535967
saqibkh <saqibkhan@utexas.edu>
2021-08-20 11:36:45 -0500

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/stock.py
+++ scripts/stock.py
@@ -62,10 +62,7 @@ class STOCK:
 
             if 'industry' in self.ticker_object.info:
                 self.industry = self.ticker_object.info['industry']
-                if isinstance(self.industry, unicode):
-                    self.industry = 
-                print(str(self.industry))
-                self.simlog.debug("industry is " + str(self.industry.encode('utf-8')))
+                self.simlog.debug("industry is " + str(self.industry))
 
             if 'market' in self.ticker_object.info:
                 self.market = self.ticker_object.info['market']
