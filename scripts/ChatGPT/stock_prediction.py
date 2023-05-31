87701309f5d8f5fe72bbcabaa162f5e5af2c25c6
saqibkh <saqibkhan@utexas.edu>
2023-05-31 15:45:18 -0500

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/ChatGPT/stock_prediction.py
+++ scripts/ChatGPT/stock_prediction.py
@@ -121,9 +121,9 @@ class STOCK_PREDICTION:
             self.simlog.info("Percentage Change = " + str(i_percentage_change))
             if float(self.master_list[i]['RMSE']) < 30:
                 if i_percentage_change:
-                    if i_percentage_change > 8:
+                    if i_percentage_change > 10:
                         i_score_buy += 1
-                    elif i_percentage_change < 3:
+                    elif i_percentage_change < 4:
                         i_score_sell += 1
                     else:
                         continue
